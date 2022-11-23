280326068aa393d2ba6ccba0a46009f4fe5a33e1
cvija1 <djordje.cvija.cvijanovic@gmail.com>
2022-11-23 21:21:46 +0100

    v3

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //zbog problema sa proxy serverom ovo se mora ubaciti da bi moglo da se salje zahtjev na drugi port
 app.use(cors());
+//serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html");
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "hello" });
+  });
+}
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello" });
@@ -23,19 +34,6 @@ app.get("/", (req, res) => {
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 
-//serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    );
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.status(200).json({ message: "hello" });
-  });
-}
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
