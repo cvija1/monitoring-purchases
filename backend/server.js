18b430d66f4974dd349cac81e452d3ef31bfabb2
cvija1 <djordje.cvija.cvijanovic@gmail.com>
2022-11-24 19:24:17 +0100

    final

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 //serve frontend, pomjereno na vrh jer ako se spusti ispod api poziva ne radi
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
-  //   app.get("*", (req, res) => {
-  //     res.sendFile(__dirname, "../", "frontend", "build", "index.html");
-  //   });
-  // } else {
-  //   app.get("/", (req, res) => {
-  //     res.status(200).json({ message: "hello" });
-  //   });
 }
 
 app.get("/", (req, res) => {
